372c95953a0dcce24363f284650cf3fea9ad2c48
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-02 21:08:14 -0300

    Implement: Ex8

---
+++ aula_dois/ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+
+using utils;
+
+/*
+Ler 3 valores (A, B e C) representando as medidas dos lados de um triângulo e escrever se
+formam ou não um triângulo. OBS: para formar um triângulo, o valor de cada lado deve ser
+menor que a soma dos outros 2 lados. */
+namespace ex8 {
+    class Program {
+        static void Main(string[] args) {
+            Form form = new Form();
+            form.add("Insira o Lado 1", "1");
+            form.add("Insira o Lado 2", "2");
+            form.add("Insira o Lado 3", "3");
+
+            Triangle t = new Triangle(form.ask());
+            Console.WriteLine(
+                "É um triangulo? {0}",
+                t.isOkey() ? "Sim" : "Não"
+            );
+        }
+    }
+}
---
+++ aula_dois/ex8/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex8 {
+    class Triangle {
+        private List<int> data;
+
+        public Triangle(Dictionary<String, String> data) {
+            this.data = new List<int>();
+            foreach(KeyValuePair<string, string> entry in data) {
+                this.data.Add(Convert.ToInt32(entry.Value));
+            }
+        }
+
+        public bool isOkey() {
+            bool isOkey = false;
+
+            for (var i = 0; i < data.Count; i++) {
+                var val = data[i];
+                var otherVals = data;
+
+                otherVals.RemoveAt(i);
+                isOkey = val > this.sum(otherVals);
+            }
+
+            return isOkey;
+        }
+
+        private int sum(List<int> values) {
+            int total = 0;
+            foreach(int v in values) {
+                total += v;
+            }
+
+            return total;
+        }
+    }
+}
